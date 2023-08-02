51936cf7cff80288725b6fa1087da061f2bd276c
Barla Ankit <barla.x88@gmail.com>
2023-08-02 22:43:29 +0530

    Display UI for app

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-"use strict";
+'use strict';
+const container = document.querySelector('.flex-container');
+const resultContainer = document.querySelector('#result-container');
+const overlay = document.querySelector('.overlay');
+let playerScore = 0;
+let computerScore = 0;
 
 //avilable options that can be choosen
-const optionsArray = ["rock", "scissors", "paper"];
+const optionsArray = ['rock', 'scissors', 'paper'];
 
 //return an interger between given range including max and min
 const getRandomNumber = (max, min) =>
@@ -24,21 +29,50 @@ const playRound = (computerSelection, playerSelection) => {
   if (playerIndex - computerIndex === 2) isPlayerWinner = true;
   if (playerIndex - computerIndex === -2) isPlayerWinner = false;
 
-  if (playerIndex === computerIndex) return "DRAW";
+  if (playerIndex === computerIndex) return 'DRAW';
 
-  if (isPlayerWinner)
+  if (isPlayerWinner) {
+    playerScore += 1;
     return `You Won! ${playerSelection} beats ${computerSelection}`;
+  }
 
+  computerScore += 1;
   return `You lost! ${computerSelection} beats ${playerSelection}`;
 };
 
-const game = () => {
-  for (let i = 1; i <= 5; i++) {
-    const playerSelection = prompt("Enter your choice");
+container.addEventListener('click', (e) => {
+  const element = e.target.closest('.flex-content');
+  if (!element) return;
+  const result = playRound(getComputerChoice(), element.dataset.choice);
+  resultContainer.textContent = result;
+  element.classList.add('active');
 
-    const result = playRound(getComputerChoice(), playerSelection);
-    console.log(result);
+  setTimeout(() => {
+    element.classList.remove('active');
+  }, 200);
+
+  setTimeout(() => {
+    resultContainer.innerHTML =
+      playerScore >= 5 || computerScore >= 5
+        ? '<button id="playAgain" onclick="playAgain()">Play Again</button>'
+        : 'Choose an Option';
+  }, 1500);
+
+  document.querySelector('#scores-player').textContent = playerScore;
+  document.querySelector('#scores-computer').textContent = computerScore;
+
+  if (playerScore >= 5 || computerScore >= 5) {
+    overlay.classList.toggle('show-overlay');
+    overlay.textContent = `YOU ${playerScore >= 5 ? 'WON' : 'LOST'}!`;
   }
-};
+});
 
-game();
+const playAgain = () => {
+  playerScore = 0;
+  computerScore = 0;
+  overlay.textContent = '';
+  resultContainer.innerHTML = 'Choose an Option';
+  overlay.classList.toggle('show-overlay');
+  document.querySelector('#scores-player').textContent = playerScore;
+  document.querySelector('#scores-computer').textContent = computerScore;
+};
